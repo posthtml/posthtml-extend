34906c1160c17f5469ec150168f3052b7a47c5ce
Ivan Demidov <Scrum@list.ru>
2020-11-13 12:27:48 +0300

    style: after lint

--- src/index.js
+++ src/index.js
@@ -1,148 +1,148 @@
 import fs from 'fs';
 import path from 'path';
-import util from 'util';
+import {format} from 'util';
 import parseToPostHtml from 'posthtml-parser';
-import { match } from 'posthtml/lib/api';
+import {match} from 'posthtml/lib/api';
 
 const errors = {
-    'EXTENDS_NO_SRC': '<extends> has no "src"',
-    'BLOCK_NO_NAME': '<block> has no "name"',
-    'UNEXPECTED_BLOCK': 'Unexpected block "%s"'
+  EXTENDS_NO_SRC: '<extends> has no "src"',
+  BLOCK_NO_NAME: '<block> has no "name"',
+  UNEXPECTED_BLOCK: 'Unexpected block "%s"'
 };
 
-export default (options = {}) => {
-    return tree => {
-        options.encoding = options.encoding || 'utf8';
-        options.root = options.root || './';
-        options.plugins = options.plugins || [];
-        options.strict = Object.prototype.hasOwnProperty.call(options, 'strict') ? !!options.strict : true;
-        options.slotTagName = options.slotTagName || 'block';
-        options.fillTagName = options.fillTagName || 'block';
-        options.tagName = options.tagName || 'extends';
+const extend = (options = {}) => tree => {
+  options.encoding = options.encoding || 'utf8';
+  options.root = options.root || './';
+  options.plugins = options.plugins || [];
+  options.strict = Object.prototype.hasOwnProperty.call(options, 'strict') ? Boolean(options.strict) : true;
+  options.slotTagName = options.slotTagName || 'block';
+  options.fillTagName = options.fillTagName || 'block';
+  options.tagName = options.tagName || 'extends';
 
-        tree = handleExtendsNodes(tree, options, tree.messages);
+  tree = handleExtendsNodes(tree, options, tree.messages);
 
-        const blockNodes = getBlockNodes(tree, options.slotTagName);
-        for (let blockName of Object.keys(blockNodes)) {
-            let blockNode = blockNodes[blockName];
-            blockNode.tag = false;
-            blockNode.content = blockNode.content || [];
-            blockNodes[blockName] = blockNode;
-        }
+  const blockNodes = getBlockNodes(options.slotTagName, tree);
+  for (const blockName of Object.keys(blockNodes)) {
+    const blockNode = blockNodes[blockName];
+    blockNode.tag = false;
+    blockNode.content = blockNode.content || [];
+    blockNodes[blockName] = blockNode;
+  }
 
-        return tree;
-    };
+  return tree;
 };
 
 function handleExtendsNodes(tree, options, messages) {
-    match.call(tree = applyPluginsToTree(tree, options.plugins), {tag: options.tagName}, extendsNode => {
-        if (! extendsNode.attrs || ! extendsNode.attrs.src) {
-            throw getError(errors.EXTENDS_NO_SRC);
-        }
+  match.call(tree = applyPluginsToTree(tree, options.plugins), {tag: options.tagName}, extendsNode => {
+    if (!extendsNode.attrs || !extendsNode.attrs.src) {
+      throw getError(errors.EXTENDS_NO_SRC);
+    }
 
-        const layoutPath = path.resolve(options.root, extendsNode.attrs.src);
-        const layoutHtml = fs.readFileSync(layoutPath, options.encoding);
-        let layoutTree = handleExtendsNodes(applyPluginsToTree(parseToPostHtml(layoutHtml), options.plugins), options, messages);
+    const layoutPath = path.resolve(options.root, extendsNode.attrs.src);
+    const layoutHtml = fs.readFileSync(layoutPath, options.encoding);
+    const layoutTree = handleExtendsNodes(applyPluginsToTree(parseToPostHtml(layoutHtml), options.plugins), options, messages);
 
-        extendsNode.tag = false;
-        extendsNode.content = mergeExtendsAndLayout(layoutTree, extendsNode, options.strict, options.slotTagName, options.fillTagName);
-        messages.push({
-            type: 'dependency',
-            file: layoutPath,
-            from: options.from
-        });
-
-        return extendsNode;
+    extendsNode.tag = false;
+    extendsNode.content = mergeExtendsAndLayout(layoutTree, extendsNode, options.strict, options.slotTagName, options.fillTagName);
+    messages.push({
+      type: 'dependency',
+      file: layoutPath,
+      from: options.from
     });
 
-    return tree;
+    return extendsNode;
+  });
+
+  return tree;
 }
 
 function applyPluginsToTree(tree, plugins) {
-    return plugins.reduce((tree, plugin) => tree = plugin(tree), tree);
+  return plugins.reduce((tree, plugin) => {
+    tree = plugin(tree);
+    return tree;
+  }, tree);
 }
-
 
 function mergeExtendsAndLayout(layoutTree, extendsNode, strictNames, slotTagName, fillTagName) {
-    const layoutBlockNodes = getBlockNodes(layoutTree, slotTagName);
-    const extendsBlockNodes = getBlockNodes(extendsNode.content, fillTagName);
+  const layoutBlockNodes = getBlockNodes(slotTagName, layoutTree);
+  const extendsBlockNodes = getBlockNodes(fillTagName, extendsNode.content);
 
-    for (let layoutBlockName of Object.keys(layoutBlockNodes)) {
-        let extendsBlockNode = extendsBlockNodes[layoutBlockName];
-        if (!extendsBlockNode) {
-            continue;
-        }
-
-        let layoutBlockNode = layoutBlockNodes[layoutBlockName];
-        layoutBlockNode.content = mergeContent(
-            extendsBlockNode.content,
-            layoutBlockNode.content,
-            getBlockType(extendsBlockNode)
-        );
-
-        delete extendsBlockNodes[layoutBlockName];
+  for (const layoutBlockName of Object.keys(layoutBlockNodes)) {
+    const extendsBlockNode = extendsBlockNodes[layoutBlockName];
+    if (!extendsBlockNode) {
+      continue;
     }
 
-    if (strictNames) {
-        for (let extendsBlockName of Object.keys(extendsBlockNodes)) {
-            throw getError(errors.UNEXPECTED_BLOCK, extendsBlockName);
-        }
-    }
+    const layoutBlockNode = layoutBlockNodes[layoutBlockName];
+    layoutBlockNode.content = mergeContent(
+      extendsBlockNode.content,
+      layoutBlockNode.content,
+      getBlockType(extendsBlockNode)
+    );
 
-    return layoutTree;
+    delete extendsBlockNodes[layoutBlockName];
+  }
+
+  if (strictNames) {
+    for (const extendsBlockName of Object.keys(extendsBlockNodes)) {
+      throw getError(errors.UNEXPECTED_BLOCK, extendsBlockName);
+    }
+  }
+
+  return layoutTree;
 }
-
 
 function mergeContent(extendBlockContent, layoutBlockContent, extendBlockType) {
-    extendBlockContent = extendBlockContent || [];
-    layoutBlockContent = layoutBlockContent || [];
+  extendBlockContent = extendBlockContent || [];
+  layoutBlockContent = layoutBlockContent || [];
 
-    switch (extendBlockType) {
-        case 'replace':
-            layoutBlockContent = extendBlockContent;
-            break;
+  switch (extendBlockType) {
+    case 'replace':
+      layoutBlockContent = extendBlockContent;
+      break;
 
-        case 'prepend':
-            layoutBlockContent = extendBlockContent.concat(layoutBlockContent);
-            break;
+    case 'prepend':
+      layoutBlockContent = extendBlockContent.concat(layoutBlockContent);
+      break;
 
-        case 'append':
-            layoutBlockContent = layoutBlockContent.concat(extendBlockContent);
-            break;
-    }
+    case 'append':
+      layoutBlockContent = layoutBlockContent.concat(extendBlockContent);
+      break;
+    default:
+      break;
+  }
 
-    return layoutBlockContent;
+  return layoutBlockContent;
 }
-
 
 function getBlockType(blockNode) {
-    let blockType = (blockNode.attrs && blockNode.attrs.type) || '';
-    blockType = blockType.toLowerCase();
-    if (['replace', 'prepend', 'append'].indexOf(blockType) === -1) {
-        blockType = 'replace';
+  let blockType = (blockNode.attrs && blockNode.attrs.type) || '';
+  blockType = blockType.toLowerCase();
+  if (!['replace', 'prepend', 'append'].includes(blockType)) {
+    blockType = 'replace';
+  }
+
+  return blockType;
+}
+
+function getBlockNodes(tag, content = []) {
+  const blockNodes = {};
+
+  match.call(content, {tag}, node => {
+    if (!node.attrs || !node.attrs.name) {
+      throw getError(errors.BLOCK_NO_NAME);
     }
 
-    return blockType;
+    blockNodes[node.attrs.name] = node;
+    return node;
+  });
+
+  return blockNodes;
 }
 
-
-function getBlockNodes(content = [], tag) {
-    let blockNodes = {};
-
-    match.call(content, {tag}, node => {
-        if (! node.attrs || ! node.attrs.name) {
-            throw getError(errors.BLOCK_NO_NAME);
-        }
-
-        blockNodes[node.attrs.name] = node;
-        return node;
-    });
-
-    return blockNodes;
+function getError(...rest) {
+  const message = format(...rest);
+  return new Error('[posthtml-extend] ' + message);
 }
 
-
-function getError() {
-    const message = util.format.apply(util, arguments);
-    return new Error('[posthtml-extend] ' + message);
-}
+export default extend;
--- xo.config.js
+++ xo.config.js
@@ -1,5 +1,9 @@
 module.exports = {
+  env: ['mocha'],
   space: true,
   rules: {
+    'unicorn/no-reduce': 'off',
+    'promise/prefer-await-to-then': 'off',
+    'no-unreachable-loop': 'off'
   }
 };
